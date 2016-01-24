2732d811a943b9f63decb6b9a56f1ed7fc6d6bfd
Jason Black <makotothedragon@gmail.com>
2016-01-23 18:43:00 -0700

    Initial commit

---
+++ src/main/java/io/makototheknight/jormungandr/Application.java
@@ -0,0 +1,14 @@
+package io.makototheknight.jormungandr;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
